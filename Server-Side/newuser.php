cff323f6a614101c2eff6804374670b63928cfca
nishilshah17 <nishil.shah17@gmail.com>
2014-03-17 21:56:03 -0400

    signup/login
    php

--- Server-Side/newuser.php
+++ Server-Side/newuser.php
@@ -2,4 +2,29 @@
 
 include 'mysqlconnect.php';
 
+$rand = FALSE;
+$firstname = $_POST['sufirstname'];
+$email = $_POST['sulastname'];
+$password = $_POST['supassword'];
+
+$password = password_hash($password, PASSWORD_DEFAULT); //encrypt password
+
+while (!$rand)
+{
+	$userID = rand(100000000, 999999999);
+	$result = mysql_query('SELECT * FROM users WHERE userID = "'.$userID.'"');
+	if(mysql_num_rows($result) == 0) {
+		$rand = TRUE;
+	} else {
+	}
+}
+
+$resultemail = mysql_query('SELECT * FROM users WHERE email = "'.$email.'"');
+if(mysql_num_rows($resultemail) > 0){
+    $userID = "Email Already In Use";
+} else {
+    mysql_query('INSERT INTO user (userID, firstname, lastname, email, hash) VALUES ("'.$userID.'", "'.$firstname.'","'.$lastname.'", "'.$email.'", "'.$password.'")');
+}
+echo json_encode(array('userID' => $userID));
+
 ?>
--- Server-Side/validatelogin.php
+++ Server-Side/validatelogin.php
@@ -2,4 +2,26 @@
 
 include 'mysqlconnect.php';
 
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$result = mysql_query('SELECT * FROM users WHERE email = "'.$email.'"');
+
+if(mysql_num_rows($result)==0)
+{
+    $message = "Invalid Email";
+} else {
+    $row = mysql_fetch_assoc($result);
+    $userpassword = $row['hash'];
+    
+    $matched = password_verify($userpassword, $hash);
+    
+    if($matched == true){
+        $message = "true";
+    } else {
+        $message = "Invalid Password";
+    }
+}
+
+echo json_encode(array("result" => $message));
 ?>
