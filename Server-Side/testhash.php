b25fb384d6282e7ab91196bef6a2366b00756fe4
nishilshah17 <nishil.shah17@gmail.com>
2014-03-19 19:01:06 -0400

    testing

--- Server-Side/testhash.php
+++ Server-Side/testhash.php
@@ -1,17 +1,35 @@
 <?php
+
 include 'mysqlconnect.php';
 require 'password.php';
+include 'getdata.php';
 
-$password = "testpassword";
-$email = "test@gmail.com";
+//$email = $json->{'email'};
+//$password = $json->{'password'};
+
+$email = "ram@.";
+$password = "qr";
 
 $result = mysql_query('SELECT * FROM users WHERE email = "'.$email.'"');
 $row = mysql_fetch_assoc($result);
 
-$matched = password_verify($password, $row['hash']);
+if(mysql_num_rows($result)==0)
+{
+    $message = "false";
+} else {
+    
+    $matched = password_verify($password, $row['hash']);
+    
+    if($matched == true){
+        $message = $row['userID'];
+    } else {
+        $message = "false";
+    }
+}
 
+$fullname = $row['firstName']." ".$row['lastName'];
 
-echo $password;
-echo $matched;
+//echo json_encode(array('userID' => $email, 'fullname' => $fullname));
 
+echo $message;
 ?>
--- Server-Side/validatelogin.php
+++ Server-Side/validatelogin.php
@@ -9,15 +9,13 @@ $password = $json->{'password'};
 
 $result = mysql_query('SELECT * FROM users WHERE email = "'.$email.'"');
 $row = mysql_fetch_assoc($result);
-    
+
 if(mysql_num_rows($result)==0)
 {
     $message = "false";
 } else {
-    $row = mysql_fetch_assoc($result);
-    $userpassword = $row['hash'];
     
-    $matched = password_verify($userpassword, $hash);
+    $matched = password_verify($password, $row['hash']);
     
     if($matched == true){
         $message = $row['userID'];
