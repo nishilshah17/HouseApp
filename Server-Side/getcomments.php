fa9d244af92eaf5bbc255957104581c6877f4d26
nishilshah17 <nishil.shah17@gmail.com>
2014-03-29 16:10:33 -0400

    getcomments

--- Server-Side/getcomments.php
+++ Server-Side/getcomments.php
@@ -7,13 +7,23 @@ $postID = $json->{'postID'};
 
 $comments = array();
 
+$resultPost = mysql_query('SELECT * FROM posts WHERE postID = "'.$postID.'"');
+$rowPost = mysql_fetch_assoc($resultPost);
+
+$userID = $rowPost['userID'];
+$resultUser = mysql_query('SELECT * FROM users WHERE userID = "'.$userID.'"');
+$rowUser = mysql_fetch_assoc($resultUser);
+$username = $rowUser['userName'];
+
+array_push($comments, array("comment" => $rowPost['post'], "username" => $username));
 
 $resultComments = mysql_query('SELECT * FROM comments WHERE postID = "'.$postID.'"');
 
-while ($rowComments = mysql_fetch_assoc($result)) {
+while ($rowComments = mysql_fetch_assoc($resultComments)) {
     
-    $resultUser = mysql_query('SELECT * FROM users WHERE userID = "'.$userID.'"');
+    $resultUser = mysql_query('SELECT * FROM users WHERE userID = "'.$rowComments['userID'].'"');
     $rowUser= mysql_fetch_assoc($resultUser);
+    
 	array_push($comments, array("comment" => $rowComments['comment'], "username" => $rowUser['userName']));
 
 }
