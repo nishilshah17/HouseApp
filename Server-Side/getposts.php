d57d7e7f37cd802751e690cce248fdfb59a3ddd8
nishilshah17 <nishil.shah17@gmail.com>
2014-04-02 19:18:53 -0400

    check if user voted php

--- Server-Side/getposts.php
+++ Server-Side/getposts.php
@@ -16,7 +16,21 @@ while ($row = mysql_fetch_assoc($result)) {
     $rowUser = mysql_fetch_assoc($resultUser);
     $username = $rowUser['userName'];
     
-	array_push($posts, array("post" => $row['post'], "postID" => $row['postID'], "postType" => $row['approved'], "stamp" => $row['stamp'], "up" => $row['upvotes'], "down" => $row['downvotes'], "username" => $username));
+    $resultVote = mysql_query('SELECT* FROM uservote WHERE userID = "'.$userID.'"');
+    
+    if(mysql_num_rows($resultVote) > 0)
+    {
+        $rowVote = mysql_fetch_assoc($resultVote);
+        if($rowVote['vote'] == 1) {
+            $userVote = "up";
+        } else if ($rowVote['vote'] == 2) {
+            $userVote = "down";
+        } 
+    } else {
+        $userVote = "none";
+    }
+    
+	array_push($posts, array("post" => $row['post'], "postID" => $row['postID'], "postType" => $row['approved'], "stamp" => $row['stamp'], "up" => $row['upvotes'], "down" => $row['downvotes'], "username" => $username, "userVote" => $userVote));
 
 }
 
