a605b41014840a48aad348523866c1a16b9ff409
nishilshah17 <nishil.shah17@gmail.com>
2014-04-02 19:39:08 -0400

    up & down votes

--- Server-Side/downvote.php
+++ Server-Side/downvote.php
@@ -4,18 +4,25 @@ include 'mysqlconnect.php';
 include 'getdata.php';
 
 $userID = $json->{'userID'};
+$userID = 497574237;
 $postID = $json->{'postID'};
+$postID = 2;
 $vote = 0;
 
 $result = mysql_query('SELECT * FROM uservote WHERE userID = "'.$userID.'" AND postID = "'.$postID.'"');
+$row = mysql_fetch_assoc($result);
+
 if(mysql_num_rows($result) == 0)
 {
     mysql_query('INSERT INTO uservote (userID, postID, vote) VALUES ("'.$userID.'", "'.$postID.'","'.$vote.'")');
+    mysql_query('INSERT INTO down = down + 1 WHERE postID = "'.$postID.'"');
 } else {
     $currentvote = $row['vote'];
+ 
     if($currentvote == 1){
         mysql_query('UPDATE uservote SET vote = "'.$vote.'" WHERE userID = "'.$userID.'" AND postID = "'.$postID.'"');
-        mysql_query('UPDATE posts SET vote = vote + 1 WHERE postID = "'.$postID.'"');
+        mysql_query('UPDATE posts SET down = down + 1 WHERE postID = "'.$postID.'"');
+        mysql_query('UPDATE posts SET up = up - 1 WHERE postID = "'.$postID.'"');
     } else {
     }
 }
--- Server-Side/upvote.php
+++ Server-Side/upvote.php
@@ -8,14 +8,19 @@ $postID = $json->{'postID'};
 $vote = 1;
 
 $result = mysql_query('SELECT * FROM uservote WHERE userID = "'.$userID.'" AND postID = "'.$postID.'"');
+$row = mysql_fetch_assoc($result);
+
 if(mysql_num_rows($result) == 0)
 {
     mysql_query('INSERT INTO uservote (userID, postID, vote) VALUES ("'.$userID.'", "'.$postID.'","'.$vote.'")');
+    mysql_query('UPDATE posts SET up = up + 1 WHERE postID = "'.$postID.'"');
 } else {
     $currentvote = $row['vote'];
+    
     if($currentvote == 0) {   
         mysql_query('UPDATE uservote SET vote = "'.$vote.'" WHERE userID = "'.$userID.'" AND postID = "'.$postID.'"');
-        mysql_query('UPDATE posts SET upvotes = upvotes + 1 WHERE postID = "'.$postID.'"');
+        mysql_query('UPDATE posts SET up = up + 1 WHERE postID = "'.$postID.'"');
+        mysql_query('UPDATE posts SET down = down - 1 WHERE postID = "'.$postID.'"');
     } else {
     }
 }
